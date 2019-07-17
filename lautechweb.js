481d3c03f8112e0fa0ad6930400c858fb22416ca
maxibrain4 <maxibrain4@gmail.com>
2019-07-17 16:12:33 +0100

    lautech website project 1.0 completed

---
+++ lautechweb.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#close").click(function(){
+      $("#vanish").toggle();
+        });
+        
+        
+});
+$(document).ready(function(){
+  $("#close2").click(function(){
+      $("#vanish2").toggle();
+        });
+        
+        
+});
